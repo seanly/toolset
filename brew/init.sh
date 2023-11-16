980b924242dea5df3e7f2db2fa20acec74b476b9
Ys Liu <seanly@opsbox.dev>
2023-11-16 14:58:49 +0000

    Add brew base image

---
+++ brew/init.sh
@@ -0,0 +1,4 @@
+# config
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
